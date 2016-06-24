9adff05df908d66ced7033209600eb0db726d367
Even Holthe <even.holthe@payex.com>
2016-06-24 15:38:09 +0200

    Finished NPM support

--- src/main/java/net/evenh/versionmonitor/application/hosts/impl/NpmHostService.java
+++ src/main/java/net/evenh/versionmonitor/application/hosts/impl/NpmHostService.java
@@ -3,6 +3,8 @@ package net.evenh.versionmonitor.application.hosts.impl;
 import net.evenh.versionmonitor.application.hosts.HostRegistry;
 import net.evenh.versionmonitor.application.hosts.HostService;
 import net.evenh.versionmonitor.application.projects.AbstractProject;
+import net.evenh.versionmonitor.application.projects.ProjectService;
+import net.evenh.versionmonitor.application.releases.ReleaseRepository;
 import net.evenh.versionmonitor.domain.projects.NpmProject;
 import net.evenh.versionmonitor.domain.projects.npm.NpmProjectRepresentation;
 import net.evenh.versionmonitor.domain.releases.Release;
@@ -17,17 +19,25 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static net.evenh.versionmonitor.domain.releases.Release.ReleaseBuilder.builder;
 
 @Service("npmHostService")
 public class NpmHostService implements HostService, InitializingBean {
-  private static final Logger log = LoggerFactory.getLogger(GitHubHostService.class);
+  private static final Logger log = LoggerFactory.getLogger(NpmHostService.class);
   private static final String npmRegistry = "http://registry.npmjs.org/";
 
   @Autowired
   private HostRegistry registry;
+
+  @Autowired
+  private ProjectService projectService;
+
+  @Autowired
+  private ReleaseRepository releases;
 
   @Autowired
   private RestTemplate http;
@@ -55,9 +65,9 @@ public class NpmHostService implements HostService, InitializingBean {
 
   @Override
   public Optional<? extends AbstractProject> getProject(String identifier) {
+    log.debug("Processing NPM project with identifier: {}", identifier);
     try {
       NpmProjectRepresentation npm = http.getForObject(npmRegistry + "/" + identifier, NpmProjectRepresentation.class);
-      log.debug("Successfully found NPM project: {}", identifier);
       return Optional.of(createNpmProject(npm, identifier));
     } catch (HttpClientErrorException e) {
       log.warn("Got error while fetching NPM project: {}", identifier, e);
@@ -67,7 +77,47 @@ public class NpmHostService implements HostService, InitializingBean {
 
   @Override
   public List<Release> check(AbstractProject project) throws Exception {
-    return null;
+    Objects.requireNonNull("Supplied NPM project cannot be null");
+
+    if (!satisfiedBy(project)) {
+      throw new IllegalArgumentException("Project is not a NPM project: " + project);
+    }
+
+    final String prefix = this.getClass().getSimpleName() + "[" + project.getIdentifier() + "]: ";
+
+    List<Release> newReleases = new ArrayList<>();
+
+    List<String> existingReleases = project.getReleases().stream()
+      .map(Release::getVersion)
+      .collect(Collectors.toList());
+
+    try {
+      Optional<? extends AbstractProject> remoteProject = getProject(project.getIdentifier());
+
+      if(!remoteProject.isPresent()) {
+        log.warn(prefix + "Could not read project {} from NPM.", project.getIdentifier());
+        return newReleases;
+      } else {
+        remoteProject.ifPresent(npmProject -> {
+
+          npmProject.getReleases().forEach(release -> {
+            if(!existingReleases.contains(release.getVersion())) {
+              releases.saveAndFlush(release);
+              newReleases.add(release);
+            }
+          });
+
+          newReleases.forEach(project::addRelease);
+          projectService.persist(project);
+        });
+      }
+    } catch (Exception e) {
+      log.warn(prefix + "Got exception while finding new releases", e);
+    }
+
+    log.debug(prefix + "Found {} new releases", newReleases.size());
+
+    return newReleases;
   }
 
   private NpmProject createNpmProject(NpmProjectRepresentation npm, String identifier) {
