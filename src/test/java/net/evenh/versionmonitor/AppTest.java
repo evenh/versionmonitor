9e442dcc29df4da166cd0179f1214a2a28033bdd
Even Holthe <even.holthe@me.com>
2017-01-05 22:18:32 +0100

    Update to latest Spring Boot (1.4.3)

--- src/test/java/net/evenh/versionmonitor/AppTest.java
+++ src/test/java/net/evenh/versionmonitor/AppTest.java
@@ -2,14 +2,12 @@ package net.evenh.versionmonitor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(JUnit4.class)
-@SpringApplicationConfiguration(classes = App.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
 public class AppTest {
   @Test
   public void should_add_more_tests() {
