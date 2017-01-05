6c98c9f8f204479c66504abbfabcb23d3468fc8d
Even Holthe <even.holthe@me.com>
2017-01-05 22:30:22 +0100

    Dumb down test

--- src/test/java/net/evenh/versionmonitor/AppTest.java
+++ src/test/java/net/evenh/versionmonitor/AppTest.java
@@ -2,12 +2,10 @@ package net.evenh.versionmonitor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runners.JUnit4;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = App.class)
+@RunWith(JUnit4.class)
 public class AppTest {
   @Test
   public void should_add_more_tests() {
