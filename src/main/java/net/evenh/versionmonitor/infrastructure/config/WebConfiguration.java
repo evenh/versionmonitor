7815929f446016e0c2d62226eb086f6c2cae531d
Even Holthe <even.holthe@me.com>
2017-03-12 14:14:00 +0100

    Remove explicit metrics configuration

--- src/main/java/net/evenh/versionmonitor/infrastructure/config/WebConfiguration.java
+++ src/main/java/net/evenh/versionmonitor/infrastructure/config/WebConfiguration.java
@@ -1,15 +1,10 @@
 package net.evenh.versionmonitor.infrastructure.config;
 
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.servlet.InstrumentedFilter;
-import com.codahale.metrics.servlets.MetricsServlet;
 import java.util.Arrays;
 import java.util.EnumSet;
 import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +22,7 @@ import org.springframework.web.filter.CorsFilter;
  * Configuration of web application with Servlet 3.0 APIs.
  */
 @Configuration
-public class WebConfigurer implements ServletContextInitializer, EmbeddedServletContainerCustomizer {
+public class WebConfigurer implements EmbeddedServletContainerCustomizer {
   private final Logger log = LoggerFactory.getLogger(WebConfigurer.class);
 
   @Autowired
@@ -36,42 +31,12 @@ public class WebConfigurer implements ServletContextInitializer, EmbeddedServlet
   @Autowired
   private VersionmonitorConfiguration props;
 
-  @Autowired(required = false)
-  private MetricRegistry metricRegistry;
-
   @Override
   public void onStartup(ServletContext servletContext) throws ServletException {
     log.info("Web application configuration, using profiles: {}", Arrays.toString(env.getActiveProfiles()));
     EnumSet<DispatcherType> disps = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.ASYNC);
-    initMetrics(servletContext, disps);
 
     log.info("Web application fully configured");
-  }
-
-  /**
-   * Initializes Metrics.
-   */
-  private void initMetrics(ServletContext servletContext, EnumSet<DispatcherType> disps) {
-    log.debug("Initializing Metrics registries");
-    servletContext.setAttribute(InstrumentedFilter.REGISTRY_ATTRIBUTE,
-      metricRegistry);
-    servletContext.setAttribute(MetricsServlet.METRICS_REGISTRY,
-      metricRegistry);
-
-    log.debug("Registering Metrics Filter");
-    FilterRegistration.Dynamic metricsFilter = servletContext.addFilter("webappMetricsFilter",
-      new InstrumentedFilter());
-
-    metricsFilter.addMappingForUrlPatterns(disps, true, "/*");
-    metricsFilter.setAsyncSupported(true);
-
-    log.debug("Registering Metrics Servlet");
-    ServletRegistration.Dynamic metricsAdminServlet =
-      servletContext.addServlet("metricsServlet", new MetricsServlet());
-
-    metricsAdminServlet.addMapping("/metrics/metrics/*");
-    metricsAdminServlet.setAsyncSupported(true);
-    metricsAdminServlet.setLoadOnStartup(2);
   }
 
   @Bean
