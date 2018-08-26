957b1c02df6251ddb5e4e088b6e45b1956219e66
Even Holthe <even.holthe@me.com>
2018-08-26 23:04:44 +0200

    Migrate to PostgreSQL

--- src/main/resources/db/migration/V1__Create_initial_entities.sql
+++ src/main/resources/db/migration/V1__Create_initial_entities.sql
@@ -1,55 +1,50 @@
-# Base tables
-CREATE TABLE `project` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) DEFAULT NULL,
-  `identifier` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create sequence hibernate_sequence;
+create sequence project_id_seq;
 
-CREATE TABLE `releases` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `released_at` datetime NOT NULL,
-  `url` varchar(255) NOT NULL,
-  `version` varchar(255) NOT NULL,
-  `project_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKkc96tfkfbo7h16txn8y7hn87` (`project_id`),
-  CONSTRAINT `FKkc96tfkfbo7h16txn8y7hn87` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `subscription` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table if not exists project (
+  id          bigserial    not null constraint project_pkey primary key,
+  description varchar(255),
+  identifier  varchar(255) not null,
+  name        varchar(255) not null
+);
 
-CREATE TABLE `project_subscriptions` (
-  `project_id` bigint(20) NOT NULL,
-  `subscriptions_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`project_id`,`subscriptions_id`),
-  KEY `FKk11tlb7u4idjxawbiuq6hacjk` (`subscriptions_id`),
-  CONSTRAINT `FKimadi3cn36jwb6i8xfg49v0se` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
-  CONSTRAINT `FKk11tlb7u4idjxawbiuq6hacjk` FOREIGN KEY (`subscriptions_id`) REFERENCES `subscription` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table if not exists releases (
+  id          bigint       not null constraint releases_pkey primary key,
+  released_at timestamp    not null,
+  url         varchar(255) not null,
+  version     varchar(255) not null,
+  project_id  bigint
+    constraint fkkc96tfkfbo7h16txn8y7hn87 references project
+);
 
-# Vendor tables
-CREATE TABLE `project_github` (
-  `id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FKcxopdmx2c654hotr912p3n3np` FOREIGN KEY (`id`) REFERENCES `project` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table if not exists subscription (
+  id         bigint       not null constraint subscription_pkey primary key,
+  identifier varchar(255) not null,
+  name       varchar(255) not null
+);
 
-CREATE TABLE `project_npm` (
-  `id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FKrgqf8x9grcsffer5e7c6bgh2p` FOREIGN KEY (`id`) REFERENCES `project` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table if not exists project_subscriptions (
+  project_id       bigint not null
+    constraint fkimadi3cn36jwb6i8xfg49v0se references project,
+  subscriptions_id bigint not null
+    constraint fkk11tlb7u4idjxawbiuq6hacjk references subscription,
+  constraint project_subscriptions_pkey
+  primary key (project_id, subscriptions_id)
+);
 
-CREATE TABLE `subscription_slack` (
-  `channel` varchar(255) DEFAULT NULL,
-  `id` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FKkuhfwhbsxxe3cm0r555a3t4bl` FOREIGN KEY (`id`) REFERENCES `subscription` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table if not exists subscription_slack (
+  channel varchar(255),
+  id      bigint not null constraint subscription_slack_pkey primary key
+  constraint fkkuhfwhbsxxe3cm0r555a3t4bl references subscription
+);
+
+create table if not exists project_github (
+  id bigint not null constraint project_github_pkey primary key
+  constraint fkcxopdmx2c654hotr912p3n3np references project
+);
+
+create table if not exists project_npm (
+  id bigint not null constraint project_npm_pkey primary key
+  constraint fkrgqf8x9grcsffer5e7c6bgh2p references project
+);
